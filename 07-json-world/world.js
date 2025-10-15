c9c3422807504d80e75b18c1036183020c8ada40
egetaktak1212 <ebtaktak@gmail.com>
2025-10-14 23:46:58 -0400

    sorry i forgot to commit after you helped fix the for loop

--- 07-json-world/world.js
+++ 07-json-world/world.js
@@ -5,5 +5,16 @@ const data = fs.readFileSync('world.json', 'utf-8');
 const regions = JSON.parse(data);
 
 for (const region of regions.regions) {
-    console.log(`${region.name} (${region.climate})`);
+    console.log(`\nWelcome to ${region.name}`);
+    for (let i = 0; i < region.towns.length; i++) {
+        if (i === 0) {
+            console.log(`Here, you can meet the wonderful people of ${region.towns[i].name}, such as:`)
+        } else {
+            console.log(`There is also the people of ${region.towns[i].name}:`)
+        }
+        for (const person of region.towns[i].notable_people) {
+            console.log(person.name)
+        }
+    }
+
 }
---
+++ test/node-test.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get("/api/randomNumber", (ren, res) => { res.send(Math.random()); });
+
+app.listen(3000);
